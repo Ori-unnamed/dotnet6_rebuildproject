b063f61dcb39b4dd94c8fce32a9e0333346fcd99
Ori-unnamed <2898067914@qq.com>
2022-04-12 15:12:23 +0800

    初始上传

---
+++ MyService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyModel;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace WebApplication1
+{
+    public interface IMyService
+    {
+        Instance = ([FromServices] IServiceProvider sp, Tinput input) => ExpressionTool.CreateMethodDelegate<Timpl, Tinput, Toutput>(method) (sp.GetService(typeof(Tsvc)) as Timpl, input);
+    }
+
+    public class MyService
+    {
+        IServiceProvider sp;
+        void test()
+        {
+            sp.GetService(typeof());
+        }
+    }
+
+    public abstract class DynamicPorxy
+    {
+        public abstract Delegate Instance { get; set; }
+    }
+    public class DynamicPorxyImpl<Tsvc, Timpl, Tinput, Toutput> : DynamicPorxy where Timpl : class where Tinput : class where Toutput : class
+    {
+        public override Delegate Instance { get; set; }
+        public DynamicPorxyImpl(MethodInfo method)
+        {
+            Instance = ([FromServices] IServiceProvider sp, Tinput input) => ExpressionTool.CreateMethodDelegate<Timpl, Tinput, Toutput>(method)(sp.GetService(typeof(Tsvc)) as Timpl, input);
+            DependencyContext.Default.CompileLibraries.Where(x => !x.Serviceable && x.Type != "package")
+                .Select(x => AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(x.Name))).SelectMany(
+                x=>x.GetTypes().Where(x=>!x.IsInterface && x.GetInterfaces().Any()))
+        }
+    }
+}
---
+++ WebRouter.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+
+
+namespace WebApplication1
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class WebRouter:Attribute
+    {
+        public string path;
+        public HttpMethod method;
+        public WebRouter(string path)
+        {
+            this.path = path;
+            this.method = HttpMethod.Post;
+        }
+        public WebRouter(string path, HttpMethod method)
+        {
+            this.path=path;
+            this.method = method; 
+            
+        }
+        
+    }
+
+    internal class ExpressionTool
+    {
+        internal static Func<TObj, Tin, Tout> CreateMethodDelegate<TObj, Tin, Tout>(MethodInfo method)
+        {
+            var mParameter = Expression.Parameter(typeof(TObj), "m");
+            var pParameter = Expression.Parameter(typeof(Tin), "p");
+            var mcExpression = Expression.Call(mParameter, method, Expression.Convert(pParameter, typeof(Tin)));
+            var reExpression = Expression.Convert(mcExpression, typeof(Tout));
+            return Expression.Lambda<Func<TObj, Tin, Tout>>(reExpression, mParameter, pParameter).Compile();
+        }
+        internal static Func<TObj, Tout> CreateMethodDelegate<TObj, Tout>(MethodInfo method)
+        {
+            var mParameter = Expression.Parameter(typeof(TObj), "m");
+            var mcExpression = Expression.Call(mParameter, method);
+            var reExpression = Expression.Convert(mcExpression, typeof(Tout));
+            return Expression.Lambda<Func<TObj, Tout>>(reExpression, mParameter).Compile();
+        }
+    }
+}
